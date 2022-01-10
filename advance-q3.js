b251225fca4b2894e18059a471a1193f2b282eb4
Ankit Kumain <kumankit10@gmail.com>
2022-01-10 17:16:29 +0530

    add reverseCharactersOFWord

---
+++ advance-q3.js
@@ -0,0 +1,13 @@
+// Given a sentence, your function should reverse the order of characters in each word, keeping same sequence of words.
+// Example:
+// Input: reverseCharactersOfWord('we are neoGrammers') –––> Output: ew era sremmarGoen
+
+
+let reverseCharactersOfWord = str=>{
+    let words = str.split(' ');
+    let newStr = words.map(word=> word.split('').reverse().join(''));
+    return newStr.join(' ')
+}
+
+//calling reverseCahractersOfword
+console.log(reverseCharactersOfWord('we are neoGrammers'))
