273294ee0830530d47150bdcae0cf64fd8218469
Ankit Kumain <kumankit10@gmail.com>
2022-01-11 19:04:18 +0530

    add toSentenceCase function

--- functions/advance-q2.js
+++ functions/advance-q2.js
@@ -2,31 +2,10 @@
 // Example:
 // Input: toSentenceCase('we are neoGrammers') ––> Output: We Are NeoGrammers
 
-let encodeString = (str, n)=>{
-  //characters range 
-  //A-Z --> 65-90
-  //a-z --> 97-122 
 
-  let newArr ="";
-
-  for(let char of str){
-    let asciiCode = char.charCodeAt(0);
-    
-     if(asciiCode > 122-n && asciiCode <= 122){
-        asciiCode = 96 + (asciiCode % (122-n));
-        newArr += String.fromCharCode(asciiCode)
-     }
-    else if(asciiCode > 90-n && asciiCode <= 90){
-       asciiCode = 64 + (asciiCode % (90-n))
-       newArr += String.fromCharCode(asciiCode)
-     }else if(asciiCode === 32){
-       asciiCode = 32
-       newArr += String.fromCharCode(asciiCode)
-     }
-    else newArr += String.fromCharCode(asciiCode + n);
-    
-  }
-return newArr
+const toSentenceCase = str=>{
+  let newStr = str.split(' ').map(word=>`${word[0].toUpperCase()}${word.slice(1,)}`) 
+  return newStr.join(' ')
 }
 
-console.log(encodeString("neogcamp ankit", 2));
+console.log(toSentenceCase('we are neoGrammers'))
