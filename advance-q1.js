7cb9a6669b4b3085bb9ec37700514895059775eb
Ankit Kumain <kumankit10@gmail.com>
2022-01-11 16:06:36 +0530

    add test case for strings having space in between

--- advance-q1.js
+++ advance-q1.js
@@ -3,29 +3,30 @@
 // Explanation: 2 represents shifting alphabets by 2 places. a –> c, b –> d, c –> e and so on.
 
 let encodeString = (str, n)=>{
-    //characters range 
-    //A-Z --> 65-90
-    //a-z --> 97-122 
- 
-    let newArr ="";
- 
-    for(let i=0;i<str.length;i++){
-      let asciiCode = str.charCodeAt(i);
-      
-       if(asciiCode > 122-n && asciiCode <= 122){
-          asciiCode = 96 + (asciiCode % (122-n));
-          newArr += String.fromCharCode(asciiCode)
-       }
-      
-       if(asciiCode > 90-n && asciiCode <= 90){
-         asciiCode = 64 + (asciiCode % (90-n))
-         newArr += String.fromCharCode(asciiCode)
-       } 
-      
-       else newArr += String.fromCharCode(asciiCode + n);
-      
-    }
-  return newArr
+  //characters range 
+  //A-Z --> 65-90
+  //a-z --> 97-122 
+
+  let newArr ="";
+
+  for(let i=0;i<str.length;i++){
+    let asciiCode = str.charCodeAt(i);
+    
+     if(asciiCode > 122-n && asciiCode <= 122){
+        asciiCode = 96 + (asciiCode % (122-n));
+        newArr += String.fromCharCode(asciiCode)
+     }
+    else if(asciiCode > 90-n && asciiCode <= 90){
+       asciiCode = 64 + (asciiCode % (90-n))
+       newArr += String.fromCharCode(asciiCode)
+     }else if(asciiCode === 32){
+       asciiCode = 32
+       newArr += String.fromCharCode(asciiCode)
+     }
+    else newArr += String.fromCharCode(asciiCode + n);
+    
+  }
+return newArr
 }
 
-console.log(encodeString("neogcamp", 2));
+console.log(encodeString("neogcamp ankit", 2));
